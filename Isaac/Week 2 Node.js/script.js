f5211bd49a9b5a4a3ac027a782195a4973308ca1
iHeslop <iheslop@optusnet.com.au>
2022-10-18 22:32:18 +1100

    Practice JS

--- Isaac/Week 2 Node.js/script.js
+++ Isaac/Week 2 Node.js/script.js
@@ -5,15 +5,15 @@ function myName(Name) {
 
 console.log(myName(' Isaac'));
 
-
+// This code is an attempt to analyze any sentence from an HTML input field
+var sentence = document.getElementById("input")
 var Sentiment = require('sentiment');
 var Sentiment = new Sentiment();
-var result = Sentiment.analyze('You are a silly person');
+var result = Sentiment.analyze(sentence);
 console.dir(result); // Score: -2, Comparitive: -0.666
 
 
-
-
+// Copied from Module
 let helloText = "say Hi";
 let check = 4;
 if (check > 3) {
@@ -22,6 +22,8 @@ console.log(hello);// "say Hello instead"
 }
 console.log(helloText)
 
+
+// This code changes the value of the array in different ways
 let cars = ['Audi', 'BMW', 'Mclaren', 'Toyota', 'Honda']
 cars[0] = 'Ferrari'
 cars[3] = 'Subaru'
