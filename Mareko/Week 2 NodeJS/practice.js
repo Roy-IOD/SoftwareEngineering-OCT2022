45fd6998c6d9b7ff0e11d91d55058a4d8a72e6bb
marekotp <marekotp@gmail.com>
2022-10-18 21:46:31 +1300

    NodeJS Changes

---
+++ Mareko/Week 2 NodeJS/practice.js
@@ -0,0 +1,34 @@
+console.log('Hello World');
+
+function helloName(name){
+    console.log ('Hello ' + name)
+}
+
+function sum1(A,B){
+    console.log (A+B)
+}
+
+// This is the multiplication example
+function sum2(a,b){
+    return a*b
+}
+
+// This is the subtraction example
+function sum3(a,b){
+    return a-b
+}
+
+// This is the division exanple
+function sum4(a,b){
+    return a/b
+}
+
+helloName("Harry")
+
+sum1(11,89)
+
+console.log(sum2(5,2))
+
+console.log(sum3(42,59))
+
+console.log(sum4(6,3))
