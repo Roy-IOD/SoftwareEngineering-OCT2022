5f806d5c48a94126de964ee7b5d1b548fc3c370f
altrdbeast <dgcook01@gmail.com>
2022-10-18 22:31:12 +1100

    edit

--- David/Week2/script.js
+++ David/Week2/script.js
@@ -57,46 +57,46 @@ function printName2 (firstName,lastName){
 
 console.log ("----")
 
-console.log (sum(5,1))
-console.log (minus(10,5))
-console.log (divide(10,5))
-console.log (multiply(5,2))
+// console.log (sum(5,1))
+// console.log (minus(10,5))
+// console.log (divide(10,5))
+// console.log (multiply(5,2))
 
-console.log ("----")
+// console.log ("----")
 
-console.log (printName("David"))
-console.log (printName2("David","Cook"))
+// console.log (printName("David"))
+// console.log (printName2("David","Cook"))
 
 
 
-let helloText = "say Hi";
-let check = 4;
-if (check > 3) {
-     let hello = "say Hello instead";
-console.log(hello);// "say Hello instead"
-}
-console.log(helloText) // hello is not defined
+// let helloText = "say Hi";
+// let check = 4;
+// if (check > 3) {
+//      let hello = "say Hello instead";
+// console.log(hello);// "say Hello instead"
+// }
+// console.log(helloText) // hello is not defined
 
-let helloText = "say Hi";
-let check = 4;
-if (check > 3) {
-     let hello = "say Hello instead";
-console.log(hello);// "say Hello instead" }
-console.log(hello) // hello is not defined
+// let helloText = "say Hi";
+// let check = 4;
+// if (check > 3) {
+//      let hello = "say Hello instead";
+// console.log(hello);// "say Hello instead" }
+// console.log(hello) // hello is not defined
 
-let mountains = ['Everest', 'Fuji', 'Nanga Parbat'];
-console.log(mountains[0]); // 'Everest'
-console.log(mountains[1]); // 'Fuji'
-console.log(mountains[2]); // 'Nanga Parbat'
+// let mountains = ['Everest', 'Fuji', 'Nanga Parbat'];
+// console.log(mountains[0]); // 'Everest'
+// console.log(mountains[1]); // 'Fuji'
+// console.log(mountains[2]); // 'Nanga Parbat'
 
-mountains[1] = "Killamanjaro"
-mountains.push('Kosciosko')
+// mountains[1] = "Killamanjaro"
+// mountains.push('Kosciosko')
 
-console.log(mountains)
+// console.log(mountains)
 
-let seas = ['Black Sea', 'Caribbean Sea', 'North Sea', 'Baltic Sea'];
-console.log(seas);
+// let seas = ['Black Sea', 'Caribbean Sea', 'North Sea', 'Baltic Sea'];
+// console.log(seas);
 
-seas.unshift('Red Sea')
-console.log(seas)
+// seas.unshift('Red Sea')
+// console.log(seas)
 
