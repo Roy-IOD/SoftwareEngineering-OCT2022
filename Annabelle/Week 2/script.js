75a16278547f99f4e96bb55d9cdafc0ecead9191
anna6e11e <annabellekoinis101@gmail.com>
2022-10-18 19:34:54 +1100

    lil work

--- Annabelle/Week 2/script.js
+++ Annabelle/Week 2/script.js
@@ -1,1 +1,12 @@
-console.log('hello world')
+function sum1(A,B){
+    return A+B
+}
+var sum=sum1(10,15)
+
+console.log(sum)
+
+function helloName(name) {
+    console.log("hello "+ name)
+}
+helloName('Annabelle')
+helloName('John')
