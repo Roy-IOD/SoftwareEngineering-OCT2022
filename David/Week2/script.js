855aae3920466949118b6564d6064486cd4c95f5
altrdbeast <dgcook01@gmail.com>
2022-10-18 22:25:13 +1100

    edit

--- David/Week2/script.js
+++ David/Week2/script.js
@@ -1,103 +1,100 @@
-// console.log('Hello World')
+console.log('Hello World')
 
-// console.log ("----")
+console.log ("----")
 
-// function sayHello(){
-//     console.log('Hello');
-//     console.log('Tuesday')
-// }
-
-
-// function sum1(a,b){
-//     console.log(a+b)
-// }
-
-// function sum2(a,b){
-//     return a+b
-// }    
-
-// sayHello()
-
-// console.log ("----")
-
-// sum1(5,2)
-// sum1(12,7)
-// sum1(478,658)
-
-// console.log(sum2(5,5))
-
-// let result =sum2(7,7)
-// console.log(result)
-
-// function sum(a,b){
-//     return a+b
-// }
-// .
-
-// function minus(a,b){
-//     return a-b
-// }
-
-// function divide(a,b){
-//     return a/b
-// }
-
-// function multiply(a,b){
-//     return a*b
-// }
-
-// function printName (name){
-//     return "Hello " + name
-// }
-
-// function printName2 (firstName,lastName){
-//     return "Hello " + firstName + " " + lastName + "," + "how are you?"
-// }
+function sayHello(){
+    console.log('Hello');
+    console.log('Tuesday')
+}
 
 
-// console.log ("----")
+function sum1(a,b){
+    console.log(a+b)
+}
 
-// console.log (sum(5,1))
-// console.log (minus(10,5))
-// console.log (divide(10,5))
-// console.log (multiply(5,2))
+function sum2(a,b){
+    return a+b
+}    
 
-// console.log ("----")
+sayHello()
 
-// console.log (printName("David"))
-// console.log (printName2("David","Cook"))
+console.log ("----")
+
+sum1(5,2)
+sum1(12,7)
+sum1(478,658)
+
+console.log(sum2(5,5))
+
+let result =sum2(7,7)
+console.log(result)
+
+function sum(a,b){
+    return a+b
+}
+
+function minus(a,b){
+    return a-b
+}
+
+function divide(a,b){
+    return a/b
+}
+
+function multiply(a,b){
+    return a*b
+}
+
+function printName (name){
+    return "Hello " + name
+}
+
+function printName2 (firstName,lastName){
+    return "Hello " + firstName + " " + lastName + "," + "how are you?"
+}
+
+
+console.log ("----")
+
+console.log (sum(5,1))
+console.log (minus(10,5))
+console.log (divide(10,5))
+console.log (multiply(5,2))
+
+console.log ("----")
+
+console.log (printName("David"))
+console.log (printName2("David","Cook"))
 
 
 
-// let helloText = "say Hi";
-// let check = 4;
-// if (check > 3) {
-//      let hello = "say Hello instead";
-// console.log(hello);// "say Hello instead"
-// }
-// console.log(helloText) // hello is not defined
+let helloText = "say Hi";
+let check = 4;
+if (check > 3) {
+     let hello = "say Hello instead";
+console.log(hello);// "say Hello instead"
+}
+console.log(helloText) // hello is not defined
 
-// let helloText = "say Hi";
-// let check = 4;
-// if (check > 3) {
-//      let hello = "say Hello instead";
-// console.log(hello);// "say Hello instead" }
-// console.log(hello) // hello is not defined
+let helloText = "say Hi";
+let check = 4;
+if (check > 3) {
+     let hello = "say Hello instead";
+console.log(hello);// "say Hello instead" }
+console.log(hello) // hello is not defined
 
-// let mountains = ['Everest', 'Fuji', 'Nanga Parbat'];
-// console.log(mountains[0]); // 'Everest'
-// console.log(mountains[1]); // 'Fuji'
-// console.log(mountains[2]); // 'Nanga Parbat'
+let mountains = ['Everest', 'Fuji', 'Nanga Parbat'];
+console.log(mountains[0]); // 'Everest'
+console.log(mountains[1]); // 'Fuji'
+console.log(mountains[2]); // 'Nanga Parbat'
 
-// mountains[1] = "Killamanjaro"
-// mountains.push('Kosciosko')
+mountains[1] = "Killamanjaro"
+mountains.push('Kosciosko')
 
-// console.log(mountains)
+console.log(mountains)
 
-// let seas = ['Black Sea', 'Caribbean Sea', 'North Sea', 'Baltic Sea'];
-// console.log(seas);
+let seas = ['Black Sea', 'Caribbean Sea', 'North Sea', 'Baltic Sea'];
+console.log(seas);
 
-// seas.unshift('Red Sea')
-// console.log(seas)
-test
-test2
+seas.unshift('Red Sea')
+console.log(seas)
