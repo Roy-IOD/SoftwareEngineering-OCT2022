10db5359101bb83db4d9969a1c9df0941ee20e24
Mopati <hagitot@yahoo.com>
2022-10-20 18:41:54 +1100

    yes please

---
+++ Mopati/Week 2 Node.js/script.js
@@ -0,0 +1,9 @@
+let mountains = ["Everest", "Fuji", "Nanga Parbat"];
+console.log(mountains[0]); // 'Everest'
+console.log(mountains[1]); // 'Fuji'
+console.log(mountains[2]); // 'Nanga Parbat'
+
+let seas = ["Black Sea", "Carribbean Sea", "North Sea", "Baltic Sea"];
+const lastElement = seas.pop();
+console.log(lastElement);
+console.log(seas);
