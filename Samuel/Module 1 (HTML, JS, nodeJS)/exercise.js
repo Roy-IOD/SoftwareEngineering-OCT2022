25ea2611ed84c49cb53bcac9e89d2adb81e4a6d8
sam-moore98 <sam98.moore@hotmail.com>
2022-10-22 14:41:15 +1100

    commit

--- Samuel/Module 1 (HTML, JS, nodeJS)/exercise.js
+++ Samuel/Module 1 (HTML, JS, nodeJS)/exercise.js
@@ -12,3 +12,15 @@ if(mySum(0,0) != 0) throw new Error('sum test 0+0 failed')
 
 
 console.log('all tests passed')
+
+// Function to generate random number 
+function randomNumber(min, max) { 
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+} 
+  
+console.log("Random Number between 1 and 10: ") 
+  
+// Function call
+console.log( randomNumber(1, 10) ); 
