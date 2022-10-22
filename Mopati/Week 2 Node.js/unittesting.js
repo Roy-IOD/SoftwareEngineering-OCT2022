c69dafb9d2314d8d2f8a2a48458579235cc23f21
Mopati <hagitot@yahoo.com>
2022-10-22 20:59:00 +1100

    yes

---
+++ Mopati/Week 2 Node.js/unittesting.js
@@ -0,0 +1,9 @@
+function sum(a, b) {
+  return a + b;
+}
+
+if (sum(9, 8) != 17) throw new Error("sum test failed");
+if (sum(-9, -8) != -17) throw new Error("sum test failed");
+if (sum(-9, 8) != -1) throw new Error("sum test failed");
+i;
+console.log(a + b);
