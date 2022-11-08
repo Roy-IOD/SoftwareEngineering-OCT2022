ae8f15779a1ba9217450b3c3fe316263a86d02f7
iHeslop <iheslop@optusnet.com.au>
2022-11-08 22:26:57 +1100

    Tues Class Update

--- Isaac/LabExercises/Module 3/script.js
+++ Isaac/LabExercises/Module 3/script.js
@@ -101,7 +101,13 @@ let ladder = {
     console.log( this.step );
     }
     };
-*/
+
 
 //Fundamentals Exercise 1.10 //
 
+let accumulator = new Accumulator(1); // initial value 1 
+accumulator.read(); // adds the user-entered value 
+accumulator.read(); // adds the user-entered value 
+console.log(accumulator.value); // shows the sum of these values
+
+*/
