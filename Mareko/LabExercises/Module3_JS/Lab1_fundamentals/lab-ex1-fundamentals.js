2404eac27acf27606fc7c7b2e593d976e7de01c6
DESKTOP-N1HG3HE\Marek <marekotp@gmail.com>
2022-11-08 23:14:08 +1300

    Mod2 Lab5 Submit + Tues Class

---
+++ Mareko/LabExercises/Module3_JS/Lab1_fundamentals/lab-ex1-fundamentals.js
@@ -0,0 +1,28 @@
+//Q1
+    /*console.log("" + 1 + 0) // this will be a string = 10
+    console.log("" - 1 + 0) //this will be a number = -1
+    console.log(true + false) //I did not know this --- 1*
+    console.log(6 / "3") // this will be a number = 2
+    console.log("2" * "3") // this will = 6
+    console.log(4 + 5 + "px") //this will = 9px 
+    console.log("$" + 4 + 5) //this will = $9 --- $45*
+    console.log("4" - 2) // this will = 2
+    console.log("4px" - 2) // this will = 2px --- NaN*
+    console.log(" -9 " + 5) // this will = -4 --- -9 5* (this will be a string)
+    console.log(" -9 " - 5) // this will = -14
+    console.log(null + 1) // this will = 1
+    console.log(undefined + 1) //this will = NaN
+    console.log(" \t \n" - 2) //I did not not this --- -2* */
+
+    /* Look into questions I answered wrong and didn't know */
+
+//Q2
+    debugger
+
+//Q3
+    console.log (5 > 4) // true (boolean)
+    console.log ("pineapples" > "pineapple") //undefined
+    "2" > "12"
+    undefined == null
+    undefined === null
+    null == "\n0\n"
