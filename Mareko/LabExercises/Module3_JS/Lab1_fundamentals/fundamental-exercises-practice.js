45ea547f9165e19cfad8ae1c735c1caa51228383
DESKTOP-N1HG3HE\Marek <marekotp@gmail.com>
2022-11-10 22:21:50 +1300

    commit

--- Mareko/LabExercises/Module3_JS/Lab1_fundamentals/fundamental-exercises-practice.js
+++ Mareko/LabExercises/Module3_JS/Lab1_fundamentals/fundamental-exercises-practice.js
@@ -26,15 +26,101 @@
 // console.log(typeof null) // "object"
 // console.log(typeof alert) // "function"
 
-const onefour = "1" + 4 //"122"
-console.log(2 * 2 - "1") //"41"
+// const onefour = "1" + 4 //"122"
+// console.log(2 * 2 - "1") //"41"
 
-const str=""
+// const str=""
 
-if(str)
-{
-    console.log('this string is true')
-}
-else (str) ;{
-        console.log('this string is false')
-    }
+// if(str)
+// {
+//     console.log('this string is true')
+// }
+// else (str) ;{
+//         console.log('this string is false')
+//     }
+
+
+// const dog = {
+//     name : 'Sparticus',
+//     breed : 'German Shepard',
+//     color : 'black',
+//     greeting: function() {
+//         console.log(`Hello, said ${ this.name } the ${ this.breed }.`);
+//     }
+// }
+
+// const dog2 = {
+//     name : 'Zeus',
+//     breed : 'Labrador',
+//     color : 'gold',
+//     greeting: function() {
+//         console.log(`Hello, said ${ this.name } the ${ this.breed }.`);
+//     }
+// }
+
+// console.log (dog)
+// console.log (dog2)
+
+// function 
+
+// constructor(newName, newBreed, newColor)
+// {
+//     this.name = newName;
+//     this.breed = newBreed;
+//     this.color = newColor;
+
+//     this.greeting = function() {
+//         console.log(`Woof, said ${ this.name } the ${ this.breed }.`);
+//     }
+// }
+
+
+
+
+// function CatConstructor(newName, newBreed, newColor) //constructor
+// {
+//     this.name = newName;
+//     this.breed = newBreed;
+//     this.color = newColor;
+
+//     this.greeting = function() {
+//         console.log(`Hello, said ${ this.name } the ${ this.breed }.`);
+//     }
+// }
+
+// const elfie = new CatConstructor('Elfie', 'Aphrodite Giant', 'ginger')
+// elfie.greeting();
+
+// class DogClass //class
+// {
+//     constructor(newName, newBreed, newColor)
+//     {
+//         this.name = newName;
+//         this.breed = newBreed;
+//         this.color = newColor;
+//     }
+
+//     greeting() {
+//         console.log(`Woof, said ${ this.name } the ${ this.breed }.`);
+//     }
+// }
+
+// const jerry = new DogClass('Jerry', 'German Shepard', 'Black')
+// jerry.greeting();
+// class CitizenClass
+// {
+//     constructor(citizenName, citizenAge, citizenCountry)
+//     {
+//         this.name = citizenName;
+//         this.age = citizenAge;
+//         this.country = citizenCountry;
+//     }
+
+//     passport() {
+//         console.log(`This is ${ this.name } they are ${ this.age } and they are from ${ this.country }.`);
+//     }
+
+// }
+
+// const pat = new CitizenClass('Pat', '25', 'Germany')
+// pat.passport();
