58c95289cd03bab7ee167373517b1fae35421b55
jobatkinIOD <jo.batkin@institutedata.com>
2022-11-15 19:27:33 +1000

    extended with more examples

--- Jo/Module3_JS/Module3LabScript.js
+++ Jo/Module3_JS/Module3LabScript.js
@@ -37,7 +37,7 @@ const companies = [
     }
 ];
 
-//This function adds a new person to the people object and prints a confirmation message
+//This function adds a new person to the END of the people object and prints a confirmation message
 function addPerson(new_name, new_street, new_suburb, new_company)
 {
     people.push({name: new_name, address: {street: new_street, suburb: new_suburb}, company: new_company});
@@ -45,10 +45,46 @@ function addPerson(new_name, new_street, new_suburb, new_company)
     console.log('added '+new_name);
 }
 
-//This function adds a new company to the companies object and prints a confirmation message
+//This function adds a new person to the BEGINNING of the people object and prints a confirmation message
+function insertPerson(new_name, new_street, new_suburb, new_company)
+{
+}
+
+//This function adds a new company to the END of the companies object and prints a confirmation message
 function addCompany(new_name, new_category, new_start, new_end)
 {
 
+}
+
+//This function adds a new company to the BEGINNING of the companies object and prints a confirmation message
+function insertCompany(new_name, new_category, new_start, new_end)
+{
+
+}
+
+//This function replaces an existing person with a new person object created from new values, returns the replaced person, and prints a confirmation message
+function replacePerson(old_name, new_name, new_street, new_suburb, new_company)
+{
+    //first get the full person details for old_name
+    let old_person = people.find((person) => person.name == old_name) //this arrow function goes through each element in the people array and returns the first one that matches the equals test
+
+    //now use that object to find the index in the people array of that person (for splicing)
+    let old_index = people.indexOf(old_person);
+
+    //create a new object for the new person
+    let new_person = {name: new_name, address: {street: new_street, suburb: new_suburb}, company: new_company}
+
+    //starting at the index of the old person, insert/replace the new one
+    people.splice(old_index, 1, new_person) 
+
+    console.log(`Replaced ${old_name} at index ${old_index} with ${new_name}`)
+
+    return old_person;
+}
+
+//This function replaces an existing company with a new company object created from new values, returns the replaced company, and prints a confirmation message
+function replaceCompany(old_name, new_category, new_start, new_end)
+{
 }
 
 //This function prints and returns how many people exist
@@ -103,11 +139,42 @@ function changeAddress(person_name, new_street, new_suburb)
 
 }
 
+//This function adds a new country property to each person (if it didnt exist already) and sets it to the given value
+function setCountry(new_country)
+{
+    people.map(person => person.address.country = new_country)
+
+    //a longer, more complete/readable way of writing the above which does exactly the same thing:
+    //people.map(function(person) {
+    //    person.address.country = new_country
+    //})
+}
+
+//This function adds a new tax_status property to each company (if it didnt exist already) and sets it to the given value
+function setTaxStatus(new_tax_status)
+{
+
+}
+
+//This function returns the given most recent people (at the end of the array)
+function getLatestPeople(latest_number)
+{
+    //so if latest_number is 2, it will start at end-2 and return the last 2 people
+    let startFrom = people.length - latest_number;
+    return people.slice(startFrom, people.length)
+}
+
+//This function returns the given most recent companies (at the end of the array)
+function getLatestCompanies(latest_number)
+{
+}
+
 //This function gets the company category for a given person and prints and returns the category value
 function getCompanyCategory(person_name)
 {
     let company;
     let category;
+    //first find the right company name for the given person
     for (let i = 0; i < people.length; i++)
     {
         if (people[i].name == person_name)
@@ -117,6 +184,7 @@ function getCompanyCategory(person_name)
     }
     console.log(person_name+' works at '+company);
 
+    //then use that company name to find the matching category
     for (let j = 0; j < companies.length; j++)
     {
         if (companies[j].name == company)
@@ -124,13 +192,42 @@ function getCompanyCategory(person_name)
             category = companies[j].category;
         }
     }
-    console.log(company+' is in the '+category+' category');
+
+    //since we're trying to 'find' the right company, a better way than the above for loop could be to use find:
+    let company_object = companies.find(comp => comp.name == company) //super short function that just returns true if the company name matches
+    let found_category = company_object.category
+
+    console.log(company+' is in the '+category+' ('+found_category+') category ');
 
     return category;
 }
 
 //This function gets the company starting year for a given person and prints and returns the value
 function getCompanyStartYear(person_name)
+{
+}
+
+
+//This function returns all companies in a given category
+function getMatchingCompanies(category_name)
+{
+    return companies.filter(company => company.category == category_name)
+
+    //a longer way of writing the exact same thing without arrow functions:
+    //return companies.filter(function(company) {
+    //    if (company.category == category_name)
+    //    {
+    //        return true
+    //    }
+    //    else
+    //    {
+    //        return false
+    //    }
+    //})
+}
+
+//This function returns all people in a given suburb
+function getMatchingPeople(suburb_name)
 {
 }
 
@@ -164,9 +261,14 @@ function generateCompanyTable()
 }
 
 
-//TODO: Create functions to remove a person; to remove a company; to filter and return all companies in a given category
+//TODO: Create functions to remove a person; to remove a company; to filter and return all companies that started after a certain year
 
+//examples of how to call and test the above functions
 addPerson('Jo', '44 Forty St', 'Darwin', 'Company Nine');
 countPeople();
 changeCompany('Jill', 'Company Five');
 getCompanyCategory('John');
+
+replacePerson('John', 'Andrew', '55 Fifty St', 'Nowhere', 'Company Seven')
+setCountry('Australia')
+console.log(people)
