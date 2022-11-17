0cc4cc807088fc2a0fd6a9239cca20af6b58de1f
thiagotaujiro <thiag_taujiro@hotmail.com>
2022-11-17 21:35:40 +1100

    17112022

---
+++ Thiago/M3P/intermediate.js
@@ -0,0 +1,20 @@
+//let point2 = 0.2
+//let point1 = 0.1
+
+//console.log(point1 + point2 == 0.3)
+
+//let fixedpoint1 = Number.parseFloat(point1).toFixed(2);
+//let fixedpoint2 = Number.parseFloat(point2).toFixed(2);
+
+//console.log( fixedpoint1 + fixedpoint2)
+
+//console.log(point1*100 + point2*100 == 0.3*100)
+
+//Arrays 
+
+//const sup = ["Rice" , "Beans" ,"Meat", "Juice"]
+//sup.push("Eggs")
+//console.log(sup)
+
+//Age
+
