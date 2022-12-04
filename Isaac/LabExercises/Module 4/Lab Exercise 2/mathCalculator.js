a4e99922b2ff743f80867d195285412b9eaeb6a6
iHeslop <iheslop@optusnet.com.au>
2022-12-04 16:25:36 +1100

    Update

--- Isaac/LabExercises/Module 4/Lab Exercise 2/mathCalculator.js
+++ Isaac/LabExercises/Module 4/Lab Exercise 2/mathCalculator.js
@@ -7,9 +7,25 @@ function mathCalculate() {
         let result = Number(firstNumber) + Number(lastNumber)
         return document.getElementById('result').innerHTML = result;
     } 
-    if (selectedOperator = "subtraction") {
+    else if (selectedOperator = "subtraction") {
         let result = Number(firstNumber) - Number(lastNumber)
         return document.getElementById('result').innerHTML = result;
     } 
-    
+    else if (selectedOperator = "divide") {
+        let result = Number(firstNumber) / Number(lastNumber)
+        return document.getElementById('result').innerHTML = result;
+    } 
+    else if (selectedOperator = "multiply") {
+        let result = Number(firstNumber) * Number(lastNumber)
+        return document.getElementById('result').innerHTML = result;
+    } 
+};
+
+function mathReset() {
+    let firstNumber = document.getElementById("fnumber")
+    let lastNumber = document.getElementById("lnumber")
+    let resetNumber = document.getElementById('result')
+    .reset(firstNumber)
+    .reset(lastNumber)
+    .reset(resetNumber)
 }
