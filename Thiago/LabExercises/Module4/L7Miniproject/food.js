38d00b13d851a509c5d81f364327f7e2ad3d2a0d
thiagotaujiro <thiag_taujiro@hotmail.com>
2022-12-22 11:16:58 +1100

    221222

---
+++ Thiago/LabExercises/Module4/L7Miniproject/food.js
@@ -0,0 +1,34 @@
+const foodChart = echarts.init(document.getElementById('foodChart'));
+
+         let foodOptions = {
+             title: {
+                 text: 'Meat consumption Australia'
+            },            
+            legend: {
+
+                orient: 'vertical',
+                 right: 10,
+                 top: 'center'
+             },
+             dataset: {
+                 source: [
+                    ['product','2019', '2020', '2021'],
+                    ['Red Meat', 89.5, 93.7, 75.2],
+                    ['White Meat', 73.4, 55.1, 84.2],
+                   ['Seafood', 65.2, 82.5, 12.4],
+                ]
+            },
+             xAxis: { type: 'category' },
+            yAxis: {},
+            series: [{ type: 'bar' }, { type: 'bar' }, { type: 'bar' }]  
+        };  
+       foodChart.setOption(foodOptions); 
+     
+       option = {
+        backgroundColor: {
+          type: 'radial',
+          x: 0.3,
+          y: 0.3,
+          r: 0.8,
+        }};
+       
