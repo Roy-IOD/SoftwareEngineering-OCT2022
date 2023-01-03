7232f99beb72156ced3cc8afee5eb452347552d0
sam-moore98 <sam98.moore@hotmail.com>
2023-01-03 19:20:30 +1100

    commit

--- Samuel/M5/myapp/index.js
+++ Samuel/M5/myapp/index.js
@@ -1,11 +1,22 @@
 const express = require('express')
 const app = express()
+const app2 = express()
 const port = 3000
+const port2 = 3001
 
 app.get('/', (req, res) => {
+    console.log(req)
     res.send('Hello World!')
+})
+
+app2.get('/', (req, res) => {
+    res.send('This is a test')
 })
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
+})
+
+app2.listen(port2, () => {
+    console.log(`Example app listening at http://localhost:${port2}`)
 })
