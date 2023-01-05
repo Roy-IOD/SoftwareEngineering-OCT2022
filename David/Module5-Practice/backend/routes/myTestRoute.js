4ddb688d9c84bcaa5a9c3885a605037e974790c2
altrdbeast <dgcook01@gmail.com>
2023-01-06 07:54:14 +1100

    thursday jan 5 exercises

--- David/Module5-Practice/backend/routes/myTestRoute.js
+++ David/Module5-Practice/backend/routes/myTestRoute.js
@@ -2,10 +2,11 @@ let express = require('express');
 let router = express.Router();
 
 router.get('/test', (req, res) => {
- res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 router.get('/test2', (req, res) => {
- res.send('Second test')
+    res.send('Second test')
 })
+
 module.exports = router;
