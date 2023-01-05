fb4f165388063d9a6a98d74af213b56fbebb8dad
jobatkinIOD <jo.batkin@institutedata.com>
2023-01-05 20:13:38 +1000

    updated module 5 examples

--- Jo/Module5_BackEnd/express-demo-app/index.js
+++ Jo/Module5_BackEnd/express-demo-app/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const testRoutes = require('./routes/myTestRoutes');
 const helloRoutes = require('./routes/hello.routes');
 const catfactsRoutes = require('./routes/catfacts.routes');
+const calculatorRoutes = require('./routes/calculatorRoutes');
 
 const app = express()
 const port = 3000
@@ -11,6 +12,7 @@ app.use('/', express.static('public'))
 app.use('/mytest', testRoutes);
 app.use('/myhello', helloRoutes);
 app.use('/catfacts', catfactsRoutes);
+app.use('/calculator', calculatorRoutes);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
