6fdcaf719da866a9ac91a81286482fcff89c6e15
Ian Ormesher <ianormesher@gmail.com>
2023-01-05 20:06:15 +1100

    thu5jan 2006

---
+++ Ian/Module5/routes/calculatorRoutes.js
@@ -0,0 +1,81 @@
+let express = require("express");
+let router = express.Router();
+
+// router.get('/add/', (req, res) => {
+//     console.log(req.query)
+//     res.send(req.query)
+// })
+
+router.get('/add', (req, res) => {
+    let number1=parseInt(req.query.num1);
+    let number2=parseInt(req.query.num2);
+
+    let sum=number1+number2
+    
+    console.log(sum)
+    
+    res.status(200)
+    res.json({result:sum})
+    })
+
+router.get('/add1', (req, res) => {
+    let number9=parseInt(req.query.num9);
+    let number10=parseInt(req.query.num10);
+
+    let sum=number9+number10
+    
+    console.log(sum)
+    
+    res.status(200)
+    res.json({result:sum})
+    })
+
+router.get('/subtract', (req, res) => {
+        let number3=parseInt(req.query.num3);
+        let number4=parseInt(req.query.num4);
+    
+        let sum=number3-number4
+        
+        console.log(sum)
+        
+        res.status(200)
+        res.json({result:sum})
+        })
+
+router.get('/subtract', (req, res) => {
+        let number3=parseInt(req.query.num3);
+            let number4=parseInt(req.query.num4);
+        
+            let sum=number3-number4
+            
+            console.log(sum)
+            
+            res.status(200)
+            res.json({result:sum})
+            })
+
+router.get('/multiply', (req, res) => {
+            let number5=parseInt(req.query.num5);
+            let number6=parseInt(req.query.num6);
+        
+            let sum=number5*number6
+            
+            console.log(sum)
+            
+            res.status(200)
+            res.json({result:sum})
+            })
+
+router.get('/divide', (req, res) => {
+                let number7=parseInt(req.query.num7);
+                let number8=parseInt(req.query.num8);
+            
+                let sum=number7/number8
+                
+                console.log(sum)
+                
+                res.status(200)
+                res.json({result:sum})
+                })
+
+module.exports = router;
