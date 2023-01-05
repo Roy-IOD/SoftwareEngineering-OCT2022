e865060f50dc50a5c7b12518186dfbe605d2eb0c
anna6e11e <annabellekoinis101@gmail.com>
2023-01-05 18:06:03 +1100

    backend

--- Annabelle/LabExercises/Module 4/managedata1.js
+++ Annabelle/LabExercises/Module 4/managedata1.js
@@ -1,10 +1,19 @@
-let news=[
+let news = [
     { id: 1, title: 'News1', content:"bla"},
     { id: 2, title: 'News2', content:"ble"},
     { id: 3, title: 'News3', content:"blu"}
-    ];
+];        
 
-    const delay = 5000;
+function addNews() {
+    
+}
 
-    news.forEach
+function addNewsItems() {
+    //this function needs to clear out the previous contents of the news-list placeholder div
+    //and then re-fill it with a cloned, populated template for each of the current items in the news array
+} 
+
+//then we call the above function every 5 seconds
+const intervalTimer = setInterval(addNewsItems, 5000)
+
 
