478eae6abeae8a9326a3abc8c8389d28a72addb2
sam-moore98 <sam98.moore@hotmail.com>
2023-01-05 19:33:07 +1100

    commit

--- Samuel/M5/myapp/routes/calculatorRoute.js
+++ Samuel/M5/myapp/routes/calculatorRoute.js
@@ -2,8 +2,12 @@ let express = require('express');
 let router = express.Router();
 
 router.get('/add', (req, res) => {
-    console.log(req.query)
-    res.send(req.query)
+    let number1=parseInt(req.query.num1);
+    let number2=parseInt(req.query.num2);
+    let sum = number1 + number2
+    console.log(sum)
+    res.status(200)
+    res.json({result:sum})
 })
 
 module.exports = router;
