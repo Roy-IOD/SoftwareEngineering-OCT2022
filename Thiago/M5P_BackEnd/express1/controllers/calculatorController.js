a2213d04cafbc172f8aee51c6293af2e7c834e2f
thiagotaujiro <thiag_taujiro@hotmail.com>
2023-01-07 10:32:54 +1100

    701231030am

---
+++ Thiago/M5P_BackEnd/express1/controllers/calculatorController.js
@@ -0,0 +1,51 @@
+const addNumbers = (req, res) => {
+    let number1 = parseInt(req.query.num1);
+    let number2 = parseInt(req.query.num2);
+
+    let sum = number1+number2
+
+    console.log(sum)
+
+    res.status(200)
+    res.json({result:sum})
+}    
+
+const subtractNumbers = (req, res) => {
+    let number1 = parseInt(req.query.num1);
+    let number2 = parseInt(req.query.num2);
+
+    let sum = number1-number2
+
+    console.log(sum)
+
+    res.status(200)
+    res.json({result:sum})
+}   
+
+const dividetNumbers = (req, res) => {
+    let number1 = parseInt(req.query.num1);
+    let number2 = parseInt(req.query.num2);
+
+    let sum = number1/number2
+
+    console.log(sum)
+
+    res.status(200)
+    res.json({result:sum})
+} 
+
+const multiplyNumbers = (req, res) => {
+    let number1 = parseInt(req.query.num1);
+    let number2 = parseInt(req.query.num2);
+
+    let sum = number1*number2
+
+    console.log(sum)
+
+    res.status(200)
+    res.json({result:sum})
+}   
+
+module.exports = {
+    addNumbers, subtractNumbers, dividetNumbers, multiplyNumbers
+}
