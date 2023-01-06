dfa4f59bfa7cef2f325c3e30bb7c22de5ae9f8a7
jobatkinIOD <jo.batkin@institutedata.com>
2023-01-07 08:41:14 +1000

    added controllers

--- Jo/Module5_BackEnd/express-demo-app/routes/calculatorRoutes.js
+++ Jo/Module5_BackEnd/express-demo-app/routes/calculatorRoutes.js
@@ -1,16 +1,9 @@
 let express = require('express');
+const calculatorController = require('../controllers/calculatorController')
 let router = express.Router();
 
 router.get('/add', (req, res) => {
-    let number1 = parseInt(req.query.num1);
-    let number2 = parseInt(req.query.num2);
-
-    let sum = number1+number2
-
-    console.log(sum)
-    
-    res.status(200)
-    res.json({result:sum})
+    calculatorController.addNumbers(req, res)
 })
 
 //add new routes for multiply, subtract, divide
--- Jo/Module5_BackEnd/express-demo-app/routes/catfacts.routes.js
+++ Jo/Module5_BackEnd/express-demo-app/routes/catfacts.routes.js
@@ -1,72 +1,19 @@
-//first run 'npm install axios'
-const axios = require('axios');
-
 const express = require('express');
-const {CatFact, CatFactsCache} = require('../models/CatFact')
+const catFactsController = require('../controllers/catfactsController')
 const router = express.Router();
 
 router.get('/random', (req, res) => {
-
-    //we can do all kinds of things within our back end, such as calling another API.
-    //fetch can be used in the browser, but the axios module is more common in backend apps
-    //see https://axios-http.com/docs/example
-    
-    axios.get('https://catfact.ninja/fact') //gets a single random cat fact
-        .then(response => {
-            console.log(response.data)
-            //send a successful response including the JSON data
-            res.status(200).json({success: true, ...response.data})
-        })
-        .catch(error => {
-            //send an error response including error details as JSON data
-            res.status(500).json({success: false, error: error.message})
-        })
+    catFactsController.randomFact(req, res);
 })
 
 //get a list of cat facts based on the limit and paging query parameters
 router.get('/list', (req, res) => {
-
-    //wrapping our own backend around someone else's lets us do things like caching 
-    //or modifying the returned JSON to suit our needs
-
-    let limit = req.query.limit ? req.query.limit : 10 //sets a default of 10 unless provided in the request
-    let page = req.query.page ? req.query.page : 1 //sets a default of 1 unless provided in the request
-    let offset = (page - 1) * limit;
-    
-    axios.get('https://catfact.ninja/facts?limit='+limit+'&page='+page) //gets a list of random cat facts
-        .then(response => {
-            //console.log(response.data)
-            const facts = response.data.data;
-            
-            //add each new fact to the cache based on the id
-            facts.forEach((fact, index) => {
-                let id = index + 1 + offset;
-                CatFactsCache.set(id, new CatFact(id, fact.fact, fact.length))
-            })
-
-            //send a successful response including the JSON data
-            res.status(200).json({success: true, facts: facts, next_page: response.data.next_page_url})
-        })
-        .catch(error => {
-            //send an error response including error details as JSON data
-            console.log(error)
-            res.status(500).json({success: false, message: error.message})
-        })
+    catFactsController.listFacts(req, res);
 })
 
 //get a specific fact from the cache based on its ID
 router.get('/fact/:id', (req, res) => {
-    console.log(req.params.id) //anything after the /fact/ in the path will be stored in a param called id
-
-    //need to parse the param to an int, since our cache map uses integer keys
-    let catfact = CatFactsCache.get(parseInt(req.params.id))
-    console.log(catfact);
-
-    if (catfact) {
-        res.status(200).json({success: true, ...catfact})
-    } else {
-        res.status(404).json({success: false, message: 'Cat fact #'+req.params.id+' not found in cache'})
-    }
+    catFactsController.getFact(req, res);
 })
 
 
