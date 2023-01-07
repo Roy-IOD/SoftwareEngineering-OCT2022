e40596b6f612e4f9f81ab64edd4091de07d6798f
sam-moore98 <sam98.moore@hotmail.com>
2023-01-07 14:47:15 +1100

    commit

--- Samuel/M5/calculator/index.js
+++ Samuel/M5/calculator/index.js
@@ -1,8 +1,18 @@
+
 const express = require('express')
 const testRoute = require('./routes/myTestRoute');
 const calculatorRoute = require('./routes/calculatorRoute');
 const app = express()
 const port = 3000
+
+const swaggerUi = require('swagger-ui-express');
+swaggerDocument = require('./swagger.json');
+app.use(
+'/api-docs',
+swaggerUi.serve,
+swaggerUi.setup(swaggerDocument)
+);
+
 
 app.use('/', express.static('public'))
 
@@ -13,4 +23,5 @@ app.use('/calculator', calculatorRoute)
 app.listen(port, () => {
       console.log(`Example app listening at http://localhost:${port}`)
 })
+
 
