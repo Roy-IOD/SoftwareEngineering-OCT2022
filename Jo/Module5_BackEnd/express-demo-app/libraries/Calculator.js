420f8a320cc5148d8e08f2bef3838ee8449c5071
jobatkinIOD <jo.batkin@institutedata.com>
2023-01-07 10:45:24 +1000

    added libraries

---
+++ Jo/Module5_BackEnd/express-demo-app/libraries/Calculator.js
@@ -0,0 +1,18 @@
+class Calculator {
+
+    constructor() {
+        this.id = Date.now()
+    }
+
+    #log=(value)=>{
+        console.log(`[Calculator:${this.id}]:${value}`)
+    }
+
+    add(num1, num2) {
+        const value=num1+num2
+        this.#log(value)
+        return value;
+    }
+}
+
+module.exports = Calculator
