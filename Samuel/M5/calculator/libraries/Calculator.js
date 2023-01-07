74d77cb5c25eca1935f5d366cc67b48174dff2ce
sam-moore98 <sam98.moore@hotmail.com>
2023-01-07 11:02:46 +1100

    commit

---
+++ Samuel/M5/calculator/libraries/Calculator.js
@@ -0,0 +1,35 @@
+
+class Calculator {
+    constructor() {
+    this.id=Date.now()
+    }
+    #log=(value)=>{
+    console.log(`[Calculator
+    :${this.id}]:${value}`)
+    }
+    add(num1, num2) {
+    const value=num1+num2
+    this.#log(value)
+    return value;
+    }
+
+    subtract(num1, num2) {
+    const value=num1-num2
+    this.#log(value)
+    return value;
+    }
+
+    multiply(num1, num2) {
+    const value=num1*num2
+    this.#log(value)
+    return value;
+    }
+
+    divide(num1, num2) {
+    const value=num1/num2
+    this.#log(value)
+    return value;
+        } 
+    }
+
+    module.exports=Calculator
