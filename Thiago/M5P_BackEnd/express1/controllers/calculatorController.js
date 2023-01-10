b7289b3df87afeb9533b50d432c9c3e7dac8444d
thiagotaujiro <thiag_taujiro@hotmail.com>
2023-01-10 20:57:55 +1100

    10/01/23

--- Thiago/M5P_BackEnd/express1/controllers/calculatorController.js
+++ Thiago/M5P_BackEnd/express1/controllers/calculatorController.js
@@ -1,51 +1,55 @@
+const Calculator = require('../libraries/Calculator')
+// const Logger = require('../libraries/Logger')
+let myCalc = new Calculator()
+// let loggingID = new Logger
+
 const addNumbers = (req, res) => {
-    let number1 = parseInt(req.query.num1);
-    let number2 = parseInt(req.query.num2);
+  let number1 = parseInt(req.query.num1);
+  let number2 = parseInt(req.query.num2);
 
-    let sum = number1+number2
+  let add = myCalc.add(number1, number2)
+  // let log = loggingID()
 
-    console.log(sum)
-
-    res.status(200)
-    res.json({result:sum})
-}    
+  res.status(200);
+  res.json({ result: add });
+};
 
 const subtractNumbers = (req, res) => {
-    let number1 = parseInt(req.query.num1);
-    let number2 = parseInt(req.query.num2);
+  let number1 = parseInt(req.query.num1);
+  let number2 = parseInt(req.query.num2);
 
-    let sum = number1-number2
+  let subtract = myCalc.subtract(number1, number2) 
 
-    console.log(sum)
-
-    res.status(200)
-    res.json({result:sum})
-}   
-
-const dividetNumbers = (req, res) => {
-    let number1 = parseInt(req.query.num1);
-    let number2 = parseInt(req.query.num2);
-
-    let sum = number1/number2
-
-    console.log(sum)
-
-    res.status(200)
-    res.json({result:sum})
-} 
+  res.status(200);
+  res.json({ result: subtract });
+};
 
 const multiplyNumbers = (req, res) => {
-    let number1 = parseInt(req.query.num1);
-    let number2 = parseInt(req.query.num2);
+  let number1 = parseInt(req.query.num1);
+  let number2 = parseInt(req.query.num2);
 
-    let sum = number1*number2
+  let multiply = myCalc.multiply(number1, number2)
 
-    console.log(sum)
+  res.status(200);
+  res.json({ result: multiply });
+};
 
-    res.status(200)
-    res.json({result:sum})
-}   
+const divideNumbers = (req, res) => {
+  let number1 = parseInt(req.query.num1);
+  let number2 = parseInt(req.query.num2);
+
+  let divide = myCalc.divide(number1, number2)
+
+  // if (Math.abs(divide) === Infinity) {
+  // throw new Error("Division by zero")}
+
+  res.status(200);
+  res.json({ result: divide });
+};
 
 module.exports = {
-    addNumbers, subtractNumbers, dividetNumbers, multiplyNumbers
-}
+  addNumbers,
+  subtractNumbers,
+  multiplyNumbers,
+  divideNumbers,
+};
