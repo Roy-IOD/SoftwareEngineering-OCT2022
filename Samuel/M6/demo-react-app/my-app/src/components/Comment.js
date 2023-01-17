7759c54f32f025d23b8732b3d18b12513f31726d
sam-moore98 <sam98.moore@hotmail.com>
2023-01-17 20:24:01 +1100

    commit

---
+++ Samuel/M6/demo-react-app/my-app/src/components/Comment.js
@@ -0,0 +1,33 @@
+
+
+function Comment(props) {
+    return (
+        <div className="Comment">
+            <UserInfo user={props.author}/>
+            <div className="Comment-text">
+                {props.text}
+            </div>
+            <div className="Comment-date">
+                {props.date.toLocaleString()}
+            </div>
+        </div>
+    
+);
+}
+
+function UserInfo(props) {
+    return(
+        <div className="UserInfo">
+            <Avatar user={props.user}/>
+            <div className="UserInfo-name">{props.user.name}</div>
+        </div>
+    );
+}
+
+function Avatar(props) {
+    return (
+        <img className="Avatar" src={props.user.avatarUrl} alt={props.user.name}/>
+    )
+}
+
+export default Comment;
