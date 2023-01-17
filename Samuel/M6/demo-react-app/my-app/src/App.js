e30eff50c6decbbd95c658add8a29c301df25d5e
sam-moore98 <sam98.moore@hotmail.com>
2023-01-17 20:23:56 +1100

    commit

--- Samuel/M6/demo-react-app/my-app/src/App.js
+++ Samuel/M6/demo-react-app/my-app/src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import MyComponent from './components/MyComponent'
+import PropsDisplayer from './components/PropsDisplayer';
+import Comment from './components/Comment';
 
 function App() {
+  const myName = 'Sam';
+  const comment = {
+      date: new Date(),
+    text: 'I hope you enjoy learning React!',
+    author: {
+      name: 'Hello Kitty',
+      avatarUrl: 'https://placekitten.com/g/64/64',
+  },
+};
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit hellooooo <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+
+      <MyComponent />
+      <PropsDisplayer test="hello" name={myName} drinks={['water','juice','coke']} number1= "4" number2= "5" />
+
+      <Comment
+        date={comment.date}
+        text={comment.text}
+        author={comment.author}
+      /> 
+ 
     </div>
   );
 }
