1cdf2a9414c4bd9e5e2c9e89e96ff08d27e30a81
Ian Ormesher <ianormesher@gmail.com>
2023-01-19 20:58:55 +1100

    thursday 19 jan 9pm

---
+++ Ian/LabExercises/Lab Exercises-Module 6/lab-exercise/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+import MyGreeting from './components/MyGreeting';
+import PropGreeting from './components/PropGreeting';
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <MyGreeting />
+        <PropGreeting />
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
