c250fbf6d6c131253e54ad9aed3786332d32c9c0
iHeslop <iheslop@optusnet.com.au>
2023-01-21 17:02:15 +1100

    Sat Class

--- Isaac/LabExercises/Module 6/Lab Exercise 1/my-app/src/App.js
+++ Isaac/LabExercises/Module 6/Lab Exercise 1/my-app/src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import MyComponent from './components/helloComponent';
+import EmojiList from './components/emojiComponent';
 
 function App() {
-  const hello = {
-    name: 'Isaac',
-  }
+  
   return ( 
     <div className="App">
-      <MyComponent name={hello.name}/>
+      <MyComponent />
+      <EmojiList />
     </div>
   );
 }
