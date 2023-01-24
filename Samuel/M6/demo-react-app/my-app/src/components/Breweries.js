4074f4e38af5a5b91ffb3ed8aab780d90fd107b3
sam-moore98 <sam98.moore@hotmail.com>
2023-01-24 20:53:52 +1100

    commit

--- Samuel/M6/demo-react-app/my-app/src/components/Breweries.js
+++ Samuel/M6/demo-react-app/my-app/src/components/Breweries.js
@@ -1,24 +1,23 @@
 import React, { Component } from 'react';
+import Pagination from './Pagination';
 
-
-//lazy loaded component which reads data from an API and therefore may cause delay when rendering
 class Breweries extends Component {
 
     constructor(props) {
         super(props);
 
-        //by default the list of breweries is empty and we're showing page 1
         this.state = {
             breweries: [],
             currentPage: 1,
-            filterType: '',
-
+            filterType: ''
         }
     }
 
-    //gets the list of breweries for the given page, then updates both the current list of breweries and the current page
     getBreweries = (page, filterType) => {
-        const filterParam = filterType === '' ? '': '&by_type='+filterType;
+        const filterParam = filterType === '' ? '' : '&by_state='+filterType;
+
+        console.log('getting '+filterType+' breweries on page number '+page)
+
         fetch('https://api.openbrewerydb.org/breweries?per_page=50&page='+page+filterParam)
         .then(response => response.json())
         .then(jsonData => {
@@ -26,13 +25,11 @@ class Breweries extends Component {
         })        
     }
 
-    //once the component is definitely being rendered on screen, load in the API data
     componentDidMount() {
-        this.getBreweries(this.state.currentPage)
+        this.getBreweries(this.state.currentPage, this.state.filterType)
     }
 
     handleChangePage = (newPage) => {
-        console.log('getting breweries on page number '+newPage)
         this.getBreweries(newPage, this.state.filterType)
     }
 
@@ -51,38 +48,32 @@ class Breweries extends Component {
                     )}
                 </ul>
                 {/* just reloads the list of breweries with the next page number */}
-                <button onClick={() => this.getBreweries(this.state.currentPage + 1)}>Next Page</button>
+                {/* <button onClick={() => this.getBreweries(this.state.currentPage + 1)}>Next Page</button> */}
 
                 {/* how could we move the paging into its own component? */}
-                {/* <Pagination currentPage={this.state.currentPage} changePageHandler={this.handleChangePage} /> */}
+                <Pagination currentPage={this.state.currentPage} changePageHandler={this.handleChangePage} />
             </div>
         );
     }
 }
 
-function Pagination(props) {
-    const prevPage = props.currentPage === 1 ? null : <button onClick={() => props.changePageHandler(props.currentPage - 1)}>Previous Page</button>;
-
-    return (
-        <div className="Pagination">
-            {prevPage}
-            <span className="current"> {props.currentPage} </span>
-            <button onClick={() => props.changePageHandler(props.currentPage + 1)}>Next Page</button>
-        </div>
-    )
-}
-
 function TypeFilter(props) {
+
+    //try adding the full list of brewery types from https://www.openbrewerydb.org/documentation here as an array and using .map() to fill the select options
+
     return (
         <div className="TypeFilter">
-            <select name="filterType" onChange={(e) => props.filterBreweryHandler(props.currentPage, e.target.value)}>
-                <option value="micro">Micro</option>
-                <option value="nano">nano</option>
-                <option value="regional">Regional</option>
+            <select name="filterType" value={props.selectedFilter} onChange={(e) => props.filterBreweryHandler(props.currentPage, e.target.value)}>
+                <option value="">Choose an option</option>
+                <option value="new york">New York</option>
+                <option value="cincinnati">California</option>
+                <option value="ohio">Ohio</option>
+                <option value="indiana">Indiana</option>
+                <option value="texas">Texas</option>
+                <option value="oklahoma">Oklahoma</option>
             </select>
         </div>
     )
 }
-
 
 export default Breweries;
