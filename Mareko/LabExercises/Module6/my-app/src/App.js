0fa87374f522a422b219fad2de52ac8e824f48e8
DESKTOP-N1HG3HE\Marek <marekotp@gmail.com>
2023-02-02 21:01:16 +1300

    commit

--- Mareko/LabExercises/Module6/my-app/src/App.js
+++ Mareko/LabExercises/Module6/my-app/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Greetings from './components/Greetings';
 import GreetingsButton from './components/GreetingsButton';
 import EmojiGenerator from './components/EmojiGenerator';
+import Calculator from './components/Calculator';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Greetings name="Mareko"/>
       <GreetingsButton />
       <EmojiGenerator />
+      <Calculator />
 
     </div>
   );
