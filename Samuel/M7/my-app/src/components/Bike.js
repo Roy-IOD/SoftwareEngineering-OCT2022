782e91bb76a77943ad37a910e495990b8f9cbd3b
sam-moore98 <sam98.moore@hotmail.com>
2023-02-02 19:32:25 +1100

    commit

---
+++ Samuel/M7/my-app/src/components/Bike.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function Bike(props) {
+    const colours = ['blue', 'black', 'red', 'yellow', 'silver']
+
+    const [make, setMake] = useState(props.make ? props.make : 'Yamaha')
+    const [model, setModel] = useState('R15')
+    const [colour, useColour] = useState('blue')
+
+    const changeBikeColour = () => {
+
+        const currentColourIndex = colours.indexOf(colour);
+
+        const nextColourIndex = currentColourIndex === colours.length-1 ? 0 : currentColourIndex + 1;
+
+        setColour(colours[nextColourIndex])
+    }
+
+    return (
+        <div className='Bike componentBox'>
+            <h2> My {make}</h2>
+
+            <p> It is a <span style={{color: colour}}>{colour}</span> {model}</p>
+
+            <button type="button" onCLick={changeBikeColour}> Change colour </button>
+        </div>
+    )
+}
+
+export default Bike;
