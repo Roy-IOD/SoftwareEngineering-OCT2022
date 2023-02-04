2e1f9fc82b62c6a7528572d8c1898d8aca7e7ac3
anna6e11e <115535673+anna6e11e@users.noreply.github.com>
2023-02-04 12:32:47 +1100

    module 7 exercise 1

---
+++ Annabelle/LabExercises/Module 7/my-app/src/components/Greeting.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+
+function Greeting(props) {
+    const [name, setName] = useState('no name')
+    const changeName = () => 
+    {let newName = name === 'no name' ? 'Annabelle' : 'no name';
+    setName(newName)
+    }
+    return (
+    
+    <div class = 'Button name change'>
+    
+    <h1>Hello {name}</h1>
+    <button onClick = {changeName}> Change name</button>
+
+    </div>
+    );
+}
+
+export default Greeting
