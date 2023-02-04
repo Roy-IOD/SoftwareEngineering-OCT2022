3b52db21330424bf53b7812778f447673652380c
sam-moore98 <sam98.moore@hotmail.com>
2023-02-04 11:05:45 +1100

    commit

--- Samuel/M7/my-app/src/App.js
+++ Samuel/M7/my-app/src/App.js
@@ -5,6 +5,7 @@ import Login from './components/Login';
 import Reservation from './components/Reservation';
 import ChatRecipientPicker from './components/ChatRecipientPicker';
 import OnlineChat from './components/OnlineChat';
+import RandomQuote from './components/RandomQuote';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
      <Reservation />
      <ChatRecipientPicker />
      <OnlineChat />
+     <RandomQuote />
 
 
  
