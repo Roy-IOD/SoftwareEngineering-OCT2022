e1e993074f61dcbf3b29e0e4f5b6b468a71f5bc2
DESKTOP-N1HG3HE\Marek <marekotp@gmail.com>
2023-02-07 20:51:28 +1300

    Commit

--- Mareko/LabExercises/Module7/my-app/src/App.js
+++ Mareko/LabExercises/Module7/my-app/src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
 import ClockDisplay from './components/Clock';
+import EmojiGenerator from './components/EmojiGenerator';
 import GreetingsButton from './components/GreetingsButton';
+import { EmojiProvider } from './context/emojiContext';
 
 function App() {
   return (
     <div className="App">
       <GreetingsButton />
-      <ClockDisplay />
+
+      <EmojiProvider>
+
+        <ClockDisplay />
+        <EmojiGenerator />
+
+      </EmojiProvider>
     </div>
   );
 }
