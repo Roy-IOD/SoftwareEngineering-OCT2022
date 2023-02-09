6af067ad45c3d820e29913a58517f52d5792a920
Ian Ormesher <ianormesher@gmail.com>
2023-02-09 21:05:46 +1100

    9pm Thursday 9 February 2023. Mui button and text field.

--- Ian/LabExercises/Lab Exercises-Module 7/react-router-app/src/App.js
+++ Ian/LabExercises/Lab Exercises-Module 7/react-router-app/src/App.js
@@ -2,12 +2,48 @@ import logo from './logo.svg';
 import './App.css';
 import { AppRoutes } from './routes/AppRoutes';
 import NavBar from './components/NavBar';
+import * as React from 'react';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import Rating from '@mui/material/Rating';
+import StarIcon from '@mui/icons-material/Star';
 
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
+export function BasicTextFields() {
+  return (
+    <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      <TextField id="filled-basic" label="Filled" variant="filled" />
+      <TextField id="standard-basic" label="Standard" variant="standard" />
+    </Box>
+  );
+}
+
+export function BasicButtons() {
+  return (
+    <Stack spacing={2} direction="row">
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+    </Stack>
+  );
+}
 
 function App() {
   return (
     <div className="App">
           <NavBar />
+          <BasicTextFields />
+          <BasicButtons />
     </div>
   );
 }
