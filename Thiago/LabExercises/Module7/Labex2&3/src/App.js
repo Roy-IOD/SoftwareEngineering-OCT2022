4128c8bfb3ace988f5bb33a7422799085e786ee9
thiagotaujiro <thiag_taujiro@hotmail.com>
2023-02-21 20:17:40 +1100

    21/02/2023

---
+++ Thiago/LabExercises/Module7/Labex2&3/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { AppRoutes } from './routes/AppRoutes';
+
+function App() {
+  return (
+    <div className="App darkMode">
+
+          <AppRoutes />
+
+    </div>
+  );
+}
+
+export default App;
