bbfffd4d21d28a2487fa28379cff425c283d20be
Ian Ormesher <ianormesher@gmail.com>
2023-02-21 21:01:46 +1100

    Tues 21 Feb

--- Ian/LabExercises/Lab Exercises-Module 7/mini-project-2/src/pages/Dashboard.js
+++ Ian/LabExercises/Lab Exercises-Module 7/mini-project-2/src/pages/Dashboard.js
@@ -1,13 +1,14 @@
 import Films from './Films';
 import Logo from '../img/Star_Wars_Logo.png';
-import Background from '../img/sw_img.jpg';
+import Background from '../img/space.jpg';
+import Image from '../img/cantina.jpg';
 
 function Dashboard() {
     return (
         <div className="dashboard-container">
         <h1>Welcome to the Cantina</h1>
         <h2>Community fan club for Star Wars fans to share, discover content and connect with other fans</h2>
-        <img src={Logo} alt="Description" />
+        <img src={Image} alt="Description" style={{width: '50%'}}/>
         </div>
     )
 }
