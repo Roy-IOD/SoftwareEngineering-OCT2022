ab962655b252114317fe4ffc21de70ae402a68bd
MopatiTt <115536571+MopatiTt@users.noreply.github.com>
2023-03-02 20:29:20 +1100

    Module 9

--- Mopati/LabExercises/Module 9/mongobd-app/models/user.js
+++ Mopati/LabExercises/Module 9/mongobd-app/models/user.js
@@ -10,12 +10,10 @@ const user = new Schema({
 });
 module.exports = mongoose.model("user", user);
 
-
 //The "user" mentioned in the above line should be in the
 //singular form ..whereas the actual collection name in
 //mongodb will be in the plural form.
-//Refer to mongoose documentation for more:
-https://www.npmjs.com/package/mongoose
+//Refer to mongoose documentation for m https://www.npmjs.com/package/mongoose
 //The first argument is the singular name of your collection.
 //Mongoose automatically looks for the lowercase plural version. For example, if you use
 //const MyModel = mongoose.model('Ticket', mySchema);
