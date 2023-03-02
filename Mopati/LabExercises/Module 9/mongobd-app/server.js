f2679077d1832ef84011911f7e0b11088486a65a
MopatiTt <115536571+MopatiTt@users.noreply.github.com>
2023-03-02 19:07:02 +1100

    module 9

--- Mopati/LabExercises/Module 9/mongobd-app/server.js
+++ Mopati/LabExercises/Module 9/mongobd-app/server.js
@@ -1,16 +1,15 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-// parse requests of content-type -
-application/json
+// parse requests of content-type -application/json
+let dbConnect = require("./dbConnect");
 app.use(express.json());
 app.get("/", (req, res) => {
-res.json({ message: "Welcome to my
-MongoDB application." });
+  res.json({ message: "Welcome to my MongoDB application." });
 });
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-console.log(`Server is running on
+  console.log(`Server is running on
 port ${PORT}.`);
 });
