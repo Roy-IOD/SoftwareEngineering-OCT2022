b96f59cb560aa66fc1788c291685e67cc13c0c40
sam-moore98 <sam98.moore@hotmail.com>
2023-03-04 10:08:36 +1100

    commit

--- Samuel/M9/mongodb-app/models/posts.js
+++ Samuel/M9/mongodb-app/models/posts.js
@@ -2,14 +2,14 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const post = new Schema({
-        postID: { type: Number, trim: true, required: true },
-        userID: { type: String, trim: true, required: true },
-        title: { type: String, trim: true, required: true, unique:true },
-        content: { type: String, trim: true, required: true },
-        image: { type: String },
-        likes: {type: Number },
-        createdAt: { type: Date, default: Date.now },
-        updatedAt: { type: Date, default: Date.now }
+  postID: { type: Number, trim: true, required: true },
+  userID: { type: String, trim: true, required: true },
+  title: { type: String, trim: true, required: true, unique: true },
+  content: { type: String, trim: true, required: true },
+  image: { type: String },
+  likes: { type: Number },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model("post", post);
