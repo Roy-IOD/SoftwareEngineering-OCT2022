167b7dbb85ed9d94a88906d74a170ec4965daf3e
sam-moore98 <sam98.moore@hotmail.com>
2023-03-04 09:13:07 +1100

    commit

--- Samuel/M9/mongodb-app/models/posts.js
+++ Samuel/M9/mongodb-app/models/posts.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const post = new Schema({
-        postId: { type: String, trim: true, required: true },
+        postId: { type: Number, trim: true, required: true },
         title: { type: String, trim: true, required: true, unique:true },
-        description: { type: String, trim: true, required: true },
+        content: { type: String, trim: true, required: true },
         image: { type: String },
         createdAt: { type: Date, default: Date.now },
         updatedAt: { type: Date, default: Date.now }
