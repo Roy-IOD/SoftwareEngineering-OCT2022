d6bae77fe2aa95e0b31ba77b76dd1f1b8c78224f
iHeslop <iheslop@optusnet.com.au>
2023-03-04 17:00:33 +1100

    Sat Class 2.0

---
+++ Isaac/Module Practice Files/Module-9/socket-chat-app/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
+
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  io.emit("connection", "a user connected");
+});
+
+server.listen(3030, () => {
+  console.log("listening on *:3030");
+});
