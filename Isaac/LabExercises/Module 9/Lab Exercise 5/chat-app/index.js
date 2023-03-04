0ecdc97ad5457af9011e0e5c2c3498cf3d936f17
iHeslop <iheslop@optusnet.com.au>
2023-03-04 17:24:25 +1100

    Sat Class 2.2

--- Isaac/LabExercises/Module 9/Lab Exercise 5/chat-app/index.js
+++ Isaac/LabExercises/Module 9/Lab Exercise 5/chat-app/index.js
@@ -2,9 +2,24 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello world</h1>");
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
 });
 
 server.listen(3001, () => {
