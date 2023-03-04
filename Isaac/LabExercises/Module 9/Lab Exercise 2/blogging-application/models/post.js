4c08e8a58af905e66e73d7d7196502a947ae154a
iHeslop <iheslop@optusnet.com.au>
2023-03-04 15:20:56 +1100

    Sat Class

--- Isaac/LabExercises/Module 9/Lab Exercise 2/blogging-application/models/post.js
+++ Isaac/LabExercises/Module 9/Lab Exercise 2/blogging-application/models/post.js
@@ -2,12 +2,13 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const post = new Schema({
-      postId: { type: Number, trim: true, required: true, unique: true },
-      title: { type: String, trim: true, required: true },
-      image: { data: Buffer, contentType: String },
-      text: { type: String, trim: true, required: true },
-      createdAt: { type: Date, default: Date.now },
-      updatedAt: { type: Date, default: Date.now }
+  userId: { type: String, trim: true, required: true },
+  title: { type: String, trim: true, required: true },
+  image: { data: Buffer, contentType: String },
+  text: { type: String, trim: true, required: true },
+  likes: { type: Number, trim: true },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model("post", post);
--- Isaac/LabExercises/Module 9/Lab Exercise 2/blogging-application/models/user.js
+++ Isaac/LabExercises/Module 9/Lab Exercise 2/blogging-application/models/user.js
@@ -5,7 +5,7 @@ const user = new Schema({
         firstName: { type: String, trim: true, required: true },
         lastName: { type: String, trim: true, required: true },
         emailId: { type: String, trim: true, required: true, unique:true },
-        password: { type: String },
+        password: { type: String, trim: true, required: true },
         createdAt: { type: Date, default: Date.now },
         updatedAt: { type: Date, default: Date.now }
 });
