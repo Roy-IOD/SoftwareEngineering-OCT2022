ec3d5c4234866f4832dce94fce73d8b1a0fbf4c9
Ian Ormesher <ianormesher@gmail.com>
2023-03-07 20:23:17 +1100

    7 mar 8.23pm

---
+++ Ian/Module9_DBAPIs/socket-chat-app/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const { Server } = require("socket.io");
+
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    io.emit('connection', 'a user connected');
+    console.log('a user connected')
+
+    socket.on('chat message', (msg) => {
+        console.log('${username}: ' + msg);
+        io.emit('chat message', msg, {username: socket.username}); //msg msg??
+    });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+
+    // socket.on('message', (msg))
+
+    socket.on('typing', (username) => {
+        socket.broadcast.emit('user typing...', username)
+    })
+
+    socket.on('set username', (username) => {
+        console.log('User ${username} has entered the chat');
+        socket.username = username;
+    });
+  });
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
