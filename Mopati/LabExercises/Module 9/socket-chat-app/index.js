d0922f24f70a293457b964ecaa98cd4b7abf42ab
MopatiTt <115536571+MopatiTt@users.noreply.github.com>
2023-03-07 20:46:31 +1100

    mod9 add user is typing

--- Mopati/LabExercises/Module 9/socket-chat-app/index.js
+++ Mopati/LabExercises/Module 9/socket-chat-app/index.js
@@ -1,15 +1,25 @@
 const express = require("express");
 const app = express();
 const http = require("http");
-const { Server } = require("socket.io");
 const server = http.createServer(app);
+const { Server } = require("socket.io");
 const io = new Server(server);
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+const usernames = [];
+app.get("/", (res, req) => {
+  req.sendFile(__dirname + "/index.html");
 });
 io.on("connection", (socket) => {
-  io.emit("connection", "a user connected");
+  socket.broadcast.emit("connection message", "a user connected...");
+  socket.on("disconnect", () => {
+    io.emit("connection message", "a user disconnected...");
+  });
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+  socket.on("typing", (username) => {
+    io.emit("typing", username);
+  });
 });
-server.listen(3000, () => {
-  console.log("listening on *:3000");
+server.listen(3001, () => {
+  console.log("listening on *:3001");
 });
