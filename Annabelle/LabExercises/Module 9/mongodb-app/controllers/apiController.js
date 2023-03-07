b129e0f70464dde1c5b16ddcc5cb1230b35209bf
anna6e11e <115535673+anna6e11e@users.noreply.github.com>
2023-03-07 20:05:51 +1100

    socket

--- Annabelle/LabExercises/Module 9/mongodb-app/controllers/apiController.js
+++ Annabelle/LabExercises/Module 9/mongodb-app/controllers/apiController.js
@@ -1,13 +1,14 @@
 "use strict";
-let models = require("../models"); //matches index.js
+let Models = require("../models"); //matches index.js
 
-const Data = require('./models/api');
-p.get('/', (req, res, next) => {
-    request('https://api.disneyapi.dev/characters', function (error, response, body) {
-        var result = JSON.parse(body);
-        console.log('body:', result); 
-        
-
+const getApis = (res) => {
+    axios.get('https://api.disneyapi.dev/characters').then(data => {
+        console.log(data.data);
+        res.send({result:200, data: data.data})
+    }).catch(err => {
+        throw err
+    })
+}
 
 module.exports = {
     getApis, createApis
