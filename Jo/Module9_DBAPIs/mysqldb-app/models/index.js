f1487bf943c21065928cb705d9e6159d6f35cc5f
jobatkinIOD <jo.batkin@institutedata.com>
2023-03-10 13:49:30 +1000

    added comments

--- Jo/Module9_DBAPIs/mysqldb-app/models/index.js
+++ Jo/Module9_DBAPIs/mysqldb-app/models/index.js
@@ -1,16 +1,17 @@
 'use strict'
+//for each Sequelize model, do the 3 steps below:
 const User = require('./user')
 const User2 = require('./user_mysql2')
 
-const Post = require('./post')
+const Post = require('./post') //1. import
 
 async function init() {
     await User.sync();
-    await Post.sync();
+    await Post.sync(); //2. sync
 };
 
 init();
 
 module.exports = {
-    User, User2, Post
+    User, User2, Post //3. export
 };
